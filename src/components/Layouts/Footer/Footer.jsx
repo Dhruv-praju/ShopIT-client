c3bd5c48c98b418bd915de104bc31e4ef8e42712
dhruv <prajapati.dp@somaiya.edu>
2022-05-06 17:09:13 +0530

    made footer

--- src/components/Layouts/Footer/Footer.jsx
+++ src/components/Layouts/Footer/Footer.jsx
@@ -1,8 +1,18 @@
+import styled from '@emotion/styled'
+import { Box,Typography } from '@mui/material'
 import React from 'react'
 
+const footer = styled('div')`
+ position: fixed,
+ bottom:0
+`
 const Footer = () => {
   return (
-    <div>Footer</div>
+      <Box sx={{position:'fixed',bottom:0, width:'100%'}}>
+        <Typography variant='body1' component='p' align='center'>
+          Shopping Cart - 2022-2023, All Rights Reserved
+        </Typography>
+      </Box>
   )
 }
 
